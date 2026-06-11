57c50e039ef3755011c879338c60a8667fc58f90
popboat1 <136786060+popboat1@users.noreply.github.com>
2026-06-11 15:12:58 +0700

    made the Value class and added addition and mult operators

--- src/autograd/Value.cpp
+++ src/autograd/Value.cpp
@@ -1,3 +1,65 @@
-#include "autograd/Value.h"
+#include "Value.h"
 
-// TODO: Implement your Value class methods here!
+Value::Value(double val) : data(val), grad(0.0), op(""), backward_func([](){}) {}
+
+Value::Value(double val, std::set<ValuePtr> children, std::string operation)
+    : data(val), grad(0.0), prev(children), op(operation), backward_func([](){}){}
+
+// addition op
+ValuePtr operator+(const ValuePtr& lhs, const ValuePtr& rhs){
+    auto out = std::make_shared<Value>(
+        lhs->data + rhs->data,
+        std::set<ValuePtr>{lhs, rhs}, "+"
+    );
+
+    //backward pass
+    out->backward_func = [lhs, rhs, out](){
+        lhs->grad += out->grad;
+        rhs->grad += out->grad;
+    };
+    return out;
+}
+
+// multiplication op
+ValuePtr operator*(const ValuePtr& lhs, const ValuePtr& rhs){
+    auto out = std::make_shared<Value>(
+        lhs->data * rhs->data,
+        std::set<ValuePtr>{lhs, rhs}, "*"
+    );
+
+    // dL/dx = y * dL/dout
+    out->backward_func = [lhs, rhs, out]() {
+        lhs->grad += rhs->data * out->grad;
+        rhs->grad += lhs->data * out->grad;
+    };
+    return out;
+}
+
+// topological sort to execute backward pass sequentially
+void Value::backward(){
+    std::vector<ValuePtr> topo;
+    std::set<ValuePtr> visited;
+
+    std::function<void(ValuePtr)> build_topo = [&](ValuePtr v) {
+        if(visited.find(v) == visited.end()){
+            visited.insert(v);
+            for(const auto& child : v->prev){
+                build_topo(child);
+            }
+            topo.push_back(v);
+        }
+    };
+
+    build_topo(shared_from_this());
+
+    this->grad = 1.0; // out node start with grad 1.0
+
+    // process nodes in reverse topo order
+    for(auto it = topo.rbegin(); it != topo.rend(); ++it){
+        (*it)->backward_func();
+    }
+}
+
+void Value::print() const {
+    std::cout << "Value(data=" << data << ", grad=" << grad << ", op='" << op << "')\n";
+}
--- src/autograd/Value.h
+++ src/autograd/Value.h
@@ -1,5 +1,40 @@
 #ifndef VALUE_H
 #define VALUE_H
 
+#include <vector>
+#include <memory>
+#include <functional>
+#include <set>
+#include <string>
+#include <iostream>
+
+class Value;
+using ValuePtr = std::shared_ptr<Value>;
+
+class Value: public std::enable_shared_from_this<Value> {
+public:
+    double data;
+    double grad;
+    std::string op;
+
+    std::set<ValuePtr> prev; // set of pointers to parent nodes that created this Value
+
+    std::function<void()> backward_func; // lambda function to pass grads back to parents
+
+    //constructors
+    Value(double val);
+    Value(double val, std::set<ValuePtr> children, std::string operation="");
+
+    void backward();
+
+    friend ValuePtr operator+(const ValuePtr& lhs, const ValuePtr& rhs);
+    friend ValuePtr operator*(const ValuePtr& lhs, const ValuePtr& rhs);
+
+    void print() const;
+};
+
+inline ValuePtr make_val(double val){
+    return std::make_shared<Value>(val);
+}
 
 #endif
